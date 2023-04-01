46d94ef9688df0367b2b8f6d1d356bbf54f4990a
Daniil Trotsenko <daniil.trotsenko@icloud.com>
2023-04-02 00:24:24 +0300

    Basics & Default/cloning/coping

--- 1_concepts/1_1_default_clone_copy/src/main.rs
+++ 1_concepts/1_1_default_clone_copy/src/main.rs
@@ -1,3 +1,42 @@
+// Auto-derive
+#[derive(Clone, Copy, Default, Debug)]
+pub struct Point {
+    pub x: i32,
+    pub y: i32,
+}
+
+#[derive(Debug)]
+pub struct PointImpl {
+    pub x: i32,
+    pub y: i32,
+}
+
+impl Default for PointImpl {
+    fn default() -> Self {
+        PointImpl { x: 0, y: 0 }
+    }
+}
+
+impl Clone for PointImpl {
+    fn clone(&self) -> Self {
+        *self
+    }
+}
+
+impl Copy for PointImpl {}
+
+#[derive(Clone, Debug)]
+pub struct Polyline {
+    pub set: Vec<Point>,
+}
+
 fn main() {
-    println!("Implement me!");
+    println!("{:?}", Point { x: 1, y: 2 });
+    println!("{:?}", PointImpl { x: 1, y: 2 });
+    println!(
+        "{:?}",
+        Polyline {
+            set: (0..100).map(|_| Point::default()).collect()
+        }
+    );
 }
