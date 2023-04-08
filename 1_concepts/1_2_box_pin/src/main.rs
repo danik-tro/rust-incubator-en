5ad728123412630585b7d5dc9e9c8a5316fa98eb
Daniil Trotsenko <daniil.trotsenko@icloud.com>
2023-04-09 00:38:09 +0300

    Implement pin for boxed value

--- 1_concepts/1_2_box_pin/src/main.rs
+++ 1_concepts/1_2_box_pin/src/main.rs
@@ -19,6 +19,15 @@ fn mut_me_somehow_string() {
     println!("{:?}", x);
 }
 
+fn mut_me_somehow_pinned_box() {
+    let mut x = Box::new(String::from("Something which will mutated in the ''"));
+
+    let x_pinned = std::pin::Pin::new(&mut x);
+    println!("{:?}", x_pinned);
+    x_pinned.mut_me_somehow();
+    println!("{:?}", x);
+}
+
 impl MutMeSomehow for String {
     fn mut_me_somehow(self: Pin<&mut Self>) {
         let i = self.get_mut();
@@ -26,6 +35,14 @@ impl MutMeSomehow for String {
     }
 }
 
+impl<T: Default> MutMeSomehow for Box<T> {
+    fn mut_me_somehow(self: Pin<&mut Self>) {
+        let pinned_box = self.get_mut().as_mut();
+        *pinned_box = T::default();
+    }
+}
+
 fn main() {
     mut_me_somehow_string();
+    mut_me_somehow_pinned_box();
 }
