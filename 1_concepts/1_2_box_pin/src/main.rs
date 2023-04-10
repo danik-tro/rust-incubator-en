7e8977adb98195e6ddad55fe32356365c00a582e
Daniil Trotsenko <daniil.trotsenko@icloud.com>
2023-04-10 21:53:06 +0300

    Add exp code

--- 1_concepts/1_2_box_pin/src/main.rs
+++ 1_concepts/1_2_box_pin/src/main.rs
@@ -1,4 +1,32 @@
-use std::pin::Pin;
+use std::{pin::Pin, rc::Rc};
+
+#[derive(Debug)]
+pub struct User<'a> {
+    pub first_name: &'a str,
+    pub last_name: &'a str,
+    pub age: u32,
+}
+
+impl<'a> User<'a> {
+    pub fn new(first_name: &'a str, last_name: &'a str, age: u32) -> Self {
+        Self {
+            first_name,
+            last_name,
+            age,
+        }
+    }
+
+    pub fn info(&self) {
+        println!(
+            "My name is: {} {}. I am {} years old",
+            self.first_name, self.last_name, self.age
+        );
+
+        println!("fn: {:p}", &self.first_name);
+        println!("ln: {:p}", &self.last_name);
+        println!("age: {:p}", &self.age);
+    }
+}
 
 trait MutMeSomehow {
     fn mut_me_somehow(self: Pin<&mut Self>);
@@ -17,6 +45,10 @@ fn mut_me_somehow_string() {
     println!("{:?}", x_pinned);
     x_pinned.mut_me_somehow();
     println!("{:?}", x);
+
+    let hi = String::from("Me");
+    let pinned_hi = Pin::new(&hi);
+    pinned_hi.say_hi();
 }
 
 fn mut_me_somehow_pinned_box() {
@@ -35,6 +67,8 @@ impl MutMeSomehow for String {
     }
 }
 
+impl SayHi for String {}
+
 impl<T: Default> MutMeSomehow for Box<T> {
     fn mut_me_somehow(self: Pin<&mut Self>) {
         let pinned_box = self.get_mut().as_mut();
@@ -42,7 +76,67 @@ impl<T: Default> MutMeSomehow for Box<T> {
     }
 }
 
+fn mem_replace_without_box() {
+    let mut user = User::new("Daniel", "Tro", 21);
+    let mut anonim = User::new("", "", 33);
+
+    println!("{:?}", user);
+    println!("User pointer {:p}", &user);
+    user.info();
+    println!("");
+
+    println!("{:?}", anonim);
+    println!("Anonim pointer {:p}", &anonim);
+    anonim.info();
+    println!("");
+
+    println!("-------------------\n");
+    std::mem::swap(&mut user, &mut anonim);
+
+    println!("{:?}", user);
+    println!("User pointer {:p}", &user);
+    user.info();
+    println!("");
+
+    println!("{:?}", anonim);
+    println!("Anonim pointer {:p}", &anonim);
+    anonim.info();
+}
+
+fn mem_replace_with_pin() {
+    let mut user = User::new("Daniel", "Tro", 21);
+    let mut anonim = User::new("", "", 33);
+
+    println!("{:?}", user);
+    println!("User pointer {:p}", &user);
+    user.info();
+    println!("");
+
+    println!("{:?}", anonim);
+    println!("Anonim pointer {:p}", &anonim);
+    anonim.info();
+    println!("");
+
+    let rc = Rc::new(&user);
+
+    let pinned = Pin::new(rc.as_ref());
+    println!("-------------------\n");
+
+    println!("{:?}", user);
+    println!("User pointer {:p}", &user);
+    user.info();
+    println!("");
+
+    println!("{:?}", anonim);
+    println!("Anonim pointer {:p}", &anonim);
+    anonim.info();
+
+    println!("{:?}", pinned)
+}
+
 fn main() {
     mut_me_somehow_string();
     mut_me_somehow_pinned_box();
+    mem_replace_without_box();
+    mem_replace_with_pin();
 }
